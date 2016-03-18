046095b6593d4f60b92da70fd2d6651fbcae01cc
Eric Drechsel <eric@pdxhub.org>
2016-03-18 11:24:52 -0700

    Build bs4 locally

---
+++ css/.src/Gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function() {
+  gulp.src('./site.scss')
+    .pipe(sass({
+      includePaths: './node_modules/bootstrap/scss/'
+    }).on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: [
+        "Android 2.3",
+        "Android >= 4",
+        "Chrome >= 20",
+        "Firefox >= 24",
+        "Explorer >= 8",
+        "iOS >= 6",
+        "Opera >= 12",
+        "Safari >= 6"
+      ],
+			cascade: false
+		}))
+    .pipe(cleanCSS({compatibility: 'ie9'}))
+    .pipe(gulp.dest('../'))
+});
+
+gulp.task('watch',function() {
+  gulp.start('styles');
+  gulp.watch('./*.scss',['styles']);
+});
+
+gulp.task('default',function() {
+  gulp.start('styles');
+});
