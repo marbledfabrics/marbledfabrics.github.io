cafd5e2b751e21c131601c122d2e01c889925104
AntonEmery <antonemery@gmail.com>
2016-11-19 20:45:00 -0800

    images showing in lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,12 +1,14 @@
-
 (function() {
-
-  $('.lightbox').click(function() {
+  function lightbox() {
+    $('.lightbox-content p').click(function () {
+      $('.overlay').remove();
+      $('.lightbox-content').remove();
+      });
+  }
+  $('.lightbox').click(function () {
     var imgSrc = $(this).attr('src');
     var fileName = imgSrc.split('/').pop();
-    
-    $('body').append('<div class="overlay"></div><div class="lightbox-content"><img src="img/full-size/' + fileName + '"></div>')
-  })
-
-})();
-
+    $('body').append('<div class="overlay"></div><div class="lightbox-content"><img src="img/full-size/' + fileName + '"><p>Close</p></div>');
+    lightbox();
+  });
+}());
